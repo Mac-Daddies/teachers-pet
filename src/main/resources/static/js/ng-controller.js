e914cb8fa46ecdcc4f6457f5900005c8035b25b9
Jessica Tracy <j.tracy916@gmail.com>
2016-10-10 13:59:08 -0400

    Login & register on front end

--- src/main/resources/static/js/ng-controller.js
+++ src/main/resources/static/js/ng-controller.js
@@ -1,6 +1,70 @@
 angular.module('TeachersPetApp', [])
    .controller('SampleController', function($scope, $http) {
 
-        $scope.message = "teacherspet!!!!!!!!!!!!!!!*****";
+        $scope.home = function() {
+            $http.get("/");
+        }
+
+        $scope.login = function(loginEmail, loginPassword) {
+            console.log("In login function in ng controller");
+
+            var emailAndPass = {
+                email: loginEmail,
+                password: loginPassword
+            }
+
+            $http.post("/login.json", emailAndPass)
+                .then(
+                    function successCallback(response) {
+                        console.log(response.data);
+                        console.log("Adding data to scope");
+                        $scope.loginContainer = response.data;
+                        if ($scope.loginContainer.errorMessage == null) {
+                            $scope.loginSuccessful = true;
+                            $scope.teacherWhoIsLoggedIn = $scope.loginContainer.teacher;
+                            console.log("User who is logged in: " + $scope.teacherWhoIsLoggedIn.firstName + ", id: " + $scope.teacherWhoIsLoggedIn.id);
+                        } else {
+                            $scope.loginSuccessful = false;
+                        }
+                    },
+                    function errorCallback(response) {
+                        console.log("Unable to get data...");
+                    });
+        };
+
+        $scope.register = function(registerFirstName, registerLastName, registerEmail, registerPassword, registerSchool) {
+                    console.log("In register function in ng controller");
+
+                    var newUserInfo = {
+                        firstName: registerFirstName,
+                        lastName: registerLastName,
+                        email: registerEmail,
+                        password: registerPassword,
+                        school: registerSchool
+                    }
+
+                    $http.post("/register.json", newUserInfo)
+                        .then(
+                            function successCallback(response) {
+                                console.log(response.data);
+                                console.log("Adding data to scope");
+                                $scope.loginContainer = response.data;
+                                if ($scope.loginContainer.errorMessage == null) {
+                                    $scope.teacherWhoIsLoggedIn = $scope.loginContainer.teacher;
+                                    console.log("User who is logged in: " + $scope.teacherWhoIsLoggedIn.firstName + ", id: " + $scope.teacherWhoIsLoggedIn.id);
+                                    $scope.loginSuccessful = true;
+                                } else {
+                                    $scope.loginSuccessful = false;
+                                }
+                            },
+                            function errorCallback(response) {
+                                console.log("Unable to get data...");
+                            });
+                };
+
+
+
+
+
 
    });
